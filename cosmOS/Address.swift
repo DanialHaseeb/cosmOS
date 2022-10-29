e60455b900631aced9dcee4cc0c4062282c707f9
Danial <syedanialhaseeb@gmail.com>
2022-10-29 17:13:31 +0500

    Updated documentation

--- cosmOS/Address.swift
+++ cosmOS/Address.swift
@@ -8,6 +8,8 @@
 /// The standard address wrapper for all memory references in **cosmOS**.
 struct Address
 {
+  // MARK: Class Constants
+  
   /// The width of the address bus defined by the virtual machine architecture.
   ///
   /// Currently set to 16 bits.
@@ -16,8 +18,9 @@ struct Address
   /// The total byte-addressable space in the main memory of our virtual machine.
   ///
   /// 16-bit addresses ⇒ 2¹⁶ uniquely addressable bytes.
-  static let space = 1 << Address.size  // ∵ 1 << n = 2^n
+  static let space = 1 << Address.size  // ∵ ∀n (1 << n  =  2ⁿ)
   
+  // MARK: Stored Constants
   
   /// The page number reference of this address.
   let page: Int
@@ -25,11 +28,15 @@ struct Address
   /// The offset of this adress.
   let offset: Int
   
+  // MARK: Initialiser
   
+  /// Converts the given word into a standard **cosmOS** address.
+  ///
+  /// - Parameter address: The word to be interpreted as an address.
   init(_ address: Word = 0)
   {
-    let address = Int(address)
-    page = address / Page.size
-    offset = address % Page.size
+    let address = Int(address)    // convert the parameter into an integer
+    page = address / Page.size    // calculate and store page number
+    offset = address % Page.size  // calculate and store offset
   }
 }
--- cosmOS/Word.swift
+++ cosmOS/Word.swift
@@ -6,4 +6,6 @@
 //
 
 /// The natural unit of data defined by the virtual machine architecture.
+///
+/// It is a 16-bit datum handled as a unit by the ISA.
 typealias Word = UInt16
